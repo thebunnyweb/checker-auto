df2837224df2c701c6238f4c975cf16a71134748
Bodo <bodo@Bodos-MacBook-Pro.local>
2022-02-10 22:03:08 +0000

    Cleanup

--- visa.js
+++ visa.js
@@ -1,10 +1,25 @@
 import Puppeteer from "puppeteer";
 
 const constants = {
-  baseUrl: "XXX",
-  userEmail: "XXX",
-  userSecret: "XXX",
+  baseUrl: "xxx",
+  userEmail: "xxx",
+  userSecret: "xxx",
+  debug: false,
 };
+
+function getLaunchParams() {
+  let options = {};
+  if (constants.debug) {
+    options = {
+      ...options,
+      headless: false,
+      devtools: true,
+    };
+  }
+  return {
+    ...options,
+  };
+}
 
 function getPayUrl(url, addr) {
   const splitUrl = url.split("/");
@@ -13,7 +28,12 @@ function getPayUrl(url, addr) {
   return payUrl;
 }
 
-async function signIn(page) {
+async function signIn(page, baseDialogClick = false) {
+  if (baseDialogClick) {
+    await page.click(
+      "body > div.ui-dialog.infoPopUp.ui-widget.ui-widget-content.ui-front.ui-dialog-buttons > div.ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix > div > button"
+    );
+  }
   await page.focus("#user_email");
   await page.keyboard.type(constants.userEmail);
   await page.waitForTimeout(500);
@@ -21,12 +41,13 @@ async function signIn(page) {
   await page.waitForTimeout(500);
   await page.keyboard.type(constants.userSecret);
   await page.waitForTimeout(500);
-  await page.click("#new_user > div.radio-checkbox-group.margin-top-30 > label");
+  await page.click(
+    "#new_user > div.radio-checkbox-group.margin-top-30 > label"
+  );
   await page.waitForTimeout(500);
-  await Promise.all([
-    page.click("#new_user > p:nth-child(9) > input"),
-    page.waitForNavigation({ waitUntil: "networkidle2" }),
-  ]);
+  await page.click("#new_user > p:nth-child(9) > input");
+  await page.waitForTimeout(1000);
+  await page.waitForNavigation({ waitUntil: "networkidle2" });
 }
 
 async function urlChecker(page) {
@@ -36,24 +57,23 @@ async function urlChecker(page) {
 }
 
 (async () => {
-  const browser = await Puppeteer.launch({
-    headless: false,
-    args: ["--incognito"],
-    devtools: false,
-  });
-
-  const page = await browser.newPage();
-  await page.setCacheEnabled(false);
-
+  const browser = await Puppeteer.launch(getLaunchParams());
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
   await page.goto(constants.baseUrl, {
-    waitUntil: "networkidle0",
+    waitUntil: "networkidle2",
   });
 
-  await signIn(page);
+  const initUrl = await page.url();
+  if (initUrl.split("/").includes("sign_in")) {
+    await signIn(page, true);
+  } else {
+    await signIn(page, false);
+  }
 
   let currentURL = await page.url();
   while (currentURL.split("/").includes("sign_in")) {
-    currentURL = urlChecker(page);
+    currentURL = await urlChecker(page); //eslint-disable-line
   }
 
   await page.click("ul.dropdown.menu.align-right.actions > li:first-child > a");
